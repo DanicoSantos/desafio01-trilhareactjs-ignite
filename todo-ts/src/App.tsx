d1b6f4661a25026f16bbe7f23cb13e5a39450b9b
Daniel Santos <dados.dan.santos@gmail.com>
2023-02-07 21:55:52 -0300

    feat: add change task status functionality

--- todo-ts/src/App.tsx
+++ todo-ts/src/App.tsx
@@ -1,14 +1,67 @@
-import { useState } from 'react'
+import { ChangeEvent, FormEvent, useState } from 'react'
 import toDoLogo from './assets/todo-logo.svg'
-
-import { NewTask } from './components/NewTask';
+import clipboard from './assets/clipboard.svg'
 
 import styles from './App.module.css';
 import './global.css';
-import { TaskBoard } from './components/TaskBoard';
-import { Task } from './components/Task';
+
+import { Task, Task as TaskInterface } from './components/Task';
+import { PlusCircle } from 'phosphor-react';
+
+
+interface TaskInterface {
+  id: string
+  content: string
+  status: 'pending' | 'finished'
+}
 
 function App() {
+
+  const [tasks, setTasks] = useState<TaskInterface[]>([])
+  const [finishedTasks, setFinishedTasks] = useState(0)
+  const [newTaskContent, setNewTaskContent] = useState('')
+
+  function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
+    setNewTaskContent(event.target.value)
+  }
+
+  function handleCreateNewTask(event: FormEvent) {
+    event.preventDefault()
+
+    const newTask: TaskInterface = {
+      id: String(new Date()),
+      content: newTaskContent,
+      status: 'pending',
+    }
+
+    setTasks([...tasks, newTask])
+    setNewTaskContent('')
+  }
+
+  const isNewTaskEmpty = newTaskContent.length === 0
+
+  function changeTaskStatus(taskId: string ) {
+    const updatedTasks = tasks.map(task => {
+      if (task.id === taskId) {
+        if (task.status === 'finished') {
+          task.status = 'pending'
+
+          setFinishedTasks(finishedTasks - 1)
+        } else {
+          task.status = 'finished'
+
+          setFinishedTasks(finishedTasks + 1)
+        }
+      }
+
+      return task
+    })
+
+    setTasks(updatedTasks)
+  }
+
+
 
   return (
     <div>
@@ -16,8 +69,50 @@ function App() {
         <img className={styles.logo} src={toDoLogo} alt='Todo App' />
       </header>
       <main className={styles.wrapper}>
-        <NewTask/>
-        <TaskBoard/>        
+        <div className={styles.newTask}>
+          <form onSubmit={handleCreateNewTask} className={styles.form}>
+            <div className={styles.formGroup}>
+              <input
+                className={styles.newTaskInput}
+                type='text'
+                placeholder='Adicione uma nova tarefa'
+                value={newTaskContent}
+                onChange={handleNewTaskChange}
+              />
+              <button type="submit" disabled={isNewTaskEmpty}>
+                Criar
+                <PlusCircle size={20} />
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className={styles.taskBoard}>
+          <header>
+            <div>Tarefas criadas <span>{tasks.length}</span></div>
+            <div>Concluídas <span>{finishedTasks}</span></div>
+          </header>
+          <main>
+            <div className={tasks.length <= 0 ? styles.emptyBoard : styles.notEmptyBoard}>
+              <img src={clipboard} />
+              <div>
+                <p>Você ainda não tem tarefas cadastradas</p>
+                <p>Crie tarefas e organize seus itens a fazer</p>
+              </div>
+            </div>
+            {tasks.map(task => {
+              return (
+                <Task 
+                  id={task.id}
+                  key={task.id} 
+                  content={task.content} 
+                  status={task.status}
+                  onChangeTaskStatus={changeTaskStatus} 
+                />
+              )
+            })}
+          </main>
+        </div>
       </main>
     </div>
   )
--- todo-ts/src/components/Task.tsx
+++ todo-ts/src/components/Task.tsx
@@ -2,17 +2,25 @@ import { Check, Trash } from 'phosphor-react'
 import styles from './Task.module.css'
 
 interface TaskProps {
+    id: string
+    content: string
     status: 'pending' | 'finished'
+    onChangeTaskStatus: (id: string) => void
 }
 
-export function Task({ status }: TaskProps) {
+export function Task({ content, status, id, onChangeTaskStatus }: TaskProps) {
+    function handleChangeTaskStatus() {
+        onChangeTaskStatus(id)
+    }
     return (
+       
         <div className={styles.task}>
             <p className={status === 'finished' ? styles.finishedTask : ''}>
-                <span className={status === 'finished' ? styles.finished : ''}>
-                    <Check className={status === 'finished' ? styles.checked : styles.unchecked}/>
+                <span onClick={handleChangeTaskStatus} className={status === 'finished' ? styles.finished : ''}>
+                    <Check className={status === 'finished' ? styles.checked : styles.unchecked} />
                 </span>
-            Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.</p>
+                {content}
+                </p>
             <span>
                 <Trash />
             </span>
